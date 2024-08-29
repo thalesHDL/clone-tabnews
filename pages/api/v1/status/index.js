4ff375353d4acff280413ee3febe52f7e377bf52
Thales Lima <thales.dev.1501@gmail.com>
2024-08-29 10:29:08 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,37 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 AS sum");
-  console.log(result.rows);
-  response.status(200).json({ status: "Ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const query = `
+    SELECT 
+      current_setting('server_version') AS server_version
+      , current_setting('max_connections') AS max_connections
+      , (SELECT sum(numbackends) FROM pg_stat_database WHERE datname = $1) AS used_connections
+    ;
+  `;
+  const result = await database.query({
+    text: query,
+    values: [databaseName],
+  });
+  const databaseVersionValue = result.rows[0].server_version;
+  const databaseMaxConnectionsValue = Number(result.rows[0].max_connections);
+  const databaseOpenedConnectionsValue =
+    Number(result.rows[0].used_connections) - 1;
+  const body = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  };
+
+  response.status(200).json(body);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,37 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  // TODO : regex para verificar padrão de versao valido
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  // expect(responseBodyDatabase.max_connections).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  // expect(responseBodyDatabase.opened_connections).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.opened_connections).toBe(0);
+
+  delete responseBody.dependencies.database.version;
+  delete responseBody.dependencies.database.max_connections;
+  delete responseBody.dependencies.database.opened_connections;
+
+  expect(Object.keys(responseBody.dependencies.database).length).toBe(0);
+
+  delete responseBody.dependencies.database;
+
+  expect(Object.keys(responseBody.dependencies).length).toBe(0);
+
+  delete responseBody.dependencies;
+  delete responseBody.updated_at;
+
+  expect(Object.keys(responseBody).length).toBe(0);
+
+  // delete responseBody.updated_at;
 });

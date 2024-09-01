1e2e1ee385ea22e996ab526e8aa1c7cc27ad0359
Thales Lima <thales.dev.1501@gmail.com>
2024-09-01 17:11:49 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,29 +1,42 @@
 import { Client } from "pg";
 
-async function query(queryObject) {
-  const client = new Client({
+async function getNewClient() {
+  const config = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
-  });
+  };
+
+  const client = new Client(config);
+
+  await client.connect();
+
+  return client;
+}
+
+async function query(queryObject) {
+  let client;
 
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    if (client) {
+      await client.end();
+    }
   }
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -33,5 +46,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
